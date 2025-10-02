7e2f7f1ad2ff08c0aa20f1bf7f1c603275b564b2
yaWhitekali <dawju9@gmail.com>
2025-10-02 20:41:54 +0200

    Update project structure and configuration files for Cash2Go application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Cash2Go</h1>
+            <p>This is your example App.tsx component.</p>
+        </div>
+    );
+}
+
+export default App;
